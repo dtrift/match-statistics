b6e1eb5a22f8578e6f439137a0021c8faecce998
dtrift <89174579997@mail.ru>
2020-04-13 20:37:28 +0500

    Added model Player

---
+++ spec/factories/players.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :player do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    number { 1 }
+  end
+end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe Player, type: :model do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :number }
+    it { should validate_numericality_of(:number).is_greater_than_or_equal_to(0) }
   end
 end

4e8caf52bc4f7bd1bb6651dea8df270696fa391d
dtrift <89174579997@mail.ru>
2020-04-19 13:11:40 +0500

    Fix

--- app/models/match_statistic.rb
+++ app/models/match_statistic.rb
@@ -6,13 +6,11 @@ class MatchStatistic < ApplicationRecord
   validates_inclusion_of :productivity, in: 0..100
   validates_inclusion_of :distance_covered, in: 0..25
 
-  scope :top_five_for, ->(team) {
-    joins(:team).where(teams: { id: team})
-      .order(productivity: :desc)
-      .first(5)
-    }
-
   scope :top_five, -> { order(productivity: :desc).first(5) }
+
+  scope :top_five_for, ->(team) {
+    joins(:team).where(teams: { id: team}).top_five
+  }
 
   scope :last_matches_for, ->(player) {
     where(player_id: player, productivity_success: true).last(5)
@@ -21,7 +19,7 @@ class MatchStatistic < ApplicationRecord
   before_save :set_stat!
 
   private
-  
+
   def self.productivity_completed_for?(player)
     last_matches_for(player).count > 0
   end
